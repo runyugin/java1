78838c7335674426046e48f9cefc241a3d86dc7e
Krasnoperov <runyugin@mail.ru>
2020-04-16 15:44:11 +0300

    HelloW1

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -3,18 +3,20 @@ package ru.progwards.java1.lessons.bitsworld;
 public class CheckBit {
 
     public static int checkBit(byte value, int bitNumber){
-
-        for (int i = 1; i < bitNumber; i++){
-
+        int resulte = 0;
+        for (int i = 1; i <= bitNumber; i++){
+            resulte = value&1;
+//            System.out.println(Integer.toBinaryString(value));
+//            System.out.println(resulte);
             value = (byte) (value>>1);
 
         }
 
-        return value&1;
+        return resulte;
     }
 
     public static void main(String[] args) {
-        checkBit((byte) 0b0100101, 3);
+        checkBit((byte) 0b01001001, 3);
     }
 
 }
